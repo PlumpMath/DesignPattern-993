e42be11700d15125805967d986d959591092615e
xmj112288 <xmj112288@163.com>
2014-07-14 12:59:35 +0800

    First Commit

---
+++ SimpleFactory/CalBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleFactory
+{
+    public abstract class CalBase
+    {
+        public double NumberA { get; set; }
+        public double NumberB { get; set; }
+
+        protected abstract double GetResult();
+    }
+
+
+    public class CalAdd : CalBase
+    {
+        protected override double GetResult()
+        {
+            return NumberA + NumberB;
+        }
+    }
+}
